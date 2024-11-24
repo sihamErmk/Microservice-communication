4755b1b8b081f06ed249a6b63ea5ee553583fdfe
EL KOUAZE Siham <selkouaze@gmail.com>
2024-11-24 16:58:21 +0100

    School controller

---
+++ School/src/main/java/com/example/school/controller/SchoolController.java
@@ -0,0 +1,30 @@
+package com.example.school.controller;
+
+import com.example.school.entity.School;
+import com.example.school.service.SchoolService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v2/School")
+public class SchoolController {
+    private final SchoolService schoolService;
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(@RequestBody School school){
+        schoolService.saveSchool(school);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<School>> findAllSchools(){
+        return ResponseEntity.ok(schoolService.findAllSchools());
+    }
+}
