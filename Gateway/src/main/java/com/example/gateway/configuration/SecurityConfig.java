6aea188d3201906962a54e6cd7014efcff9ce664
EL KOUAZE Siham <selkouaze@gmail.com>
2024-11-25 23:49:28 +0100

    last commit

--- Gateway/src/main/java/com/example/gateway/configuration/SecurityConfig.java
+++ Gateway/src/main/java/com/example/gateway/configuration/SecurityConfig.java
@@ -1,20 +1,20 @@
-package com.example.school.configuration;
+package com.example.gateway.configuration;
 
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http
+                .csrf(csrf -> csrf.disable()) // Disable CSRF protection
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // Allow all requests
         return http.build();
     }
 }
